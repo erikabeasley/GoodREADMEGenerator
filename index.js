0866e485407238e20e190c1b3fd4a1a00cbdfc84
Erika <erika@beasleys.com>
2020-08-13 20:48:03 -0400

    add questions to prompt user

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your Project Tittle?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Provide a detailed description of you project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "List table of contents",
+        name: "tableOfContents"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Choose a license ",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Enter contributing guidelines",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Provide test instructions.",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Provide test instructions.",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Enter git hub user name.",
+        name: "questionsGitHub"
+    },
+    {
+        type: "input",
+        message: "Enter email address and instructions on how to reach you with any additional questions",
+        name: "questionsEmail"
+    }
 ];
 
 // function to write README file
